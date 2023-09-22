0aa68a007d28d5698561a29a8234d694cc56def8
Arjun <vvarjun.poduval@gmail.com>
2023-09-22 13:06:00 +0530

    change1

--- MainAssessment/DTO/SeatAllocationReportRequest.cs
+++ MainAssessment/DTO/SeatAllocationReportRequest.cs
@@ -2,7 +2,6 @@
 {
     public class SeatAllocationReportRequest
     {
-        public int AllocationType { get; set; }
         public int BuildingId { get; set; }
         public int CityId { get; set; }
         public int FloorNumber { get; set; }
